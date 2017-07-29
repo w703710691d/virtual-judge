ac2bacdbe4550bd781a088e222087c5f8f1e7225
上决╇ф <w703710691d@.com>
2017-07-29 19:20:27 +0800

    修复bzoj提交

--- src/remote-provider/java/judge/remote/provider/hysbz/HYSBZSubmitter.java
+++ src/remote-provider/java/judge/remote/provider/hysbz/HYSBZSubmitter.java
@@ -10,14 +10,38 @@ import judge.remote.account.config.RemoteAccountConfig;
 import judge.remote.submitter.CanonicalSubmitter;
 import judge.remote.submitter.SubmissionInfo;
 
-import judge.tool.HtmlHandleUtil;
 import org.apache.commons.lang3.Validate;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HYSBZSubmitter extends CanonicalSubmitter {
 
+    static String getProblemUrl(HttpHost host, String problemId) {
+        return host.toURI() + "/JudgeOnline/problem.php?id=" + problemId;
+    }
+    static public void getAccess(String problemId, RemoteAccount remoteAccount, DedicatedHttpClient client) {
+        HttpHost host = new HttpHost("www.lydsy.com");
+        for(RemoteAccountConfig acc : HYSBZCrawler.acount.accounts) {
+            client.get("/JudgeOnline/logout.php", HttpStatusValidator.SC_OK);
+            HttpEntity entity = SimpleNameValueEntityFactory.create( //
+                    "user_id", acc.id, //
+                    "password", acc.password);
+            client.post("/JudgeOnline/login.php", entity, new SimpleHttpResponseValidator() {
+                @Override
+                public void validate(SimpleHttpResponse response) throws Exception {
+                    Validate.isTrue(response.getBody().contains("history.go(-2)"));
+                }
+            });
+            String problemUrl = getProblemUrl(host, problemId);
+            String html = client.get(problemUrl, HttpStatusValidator.SC_OK).getBody();
+            if(html.contains("<h2>Please contact lydsy2012@163.com!</h2>")) continue;
+            remoteAccount.setAccountId(acc.id);
+            remoteAccount.setPassword(acc.password);
+            return;
+        }
+    }
     @Override
     public RemoteOjInfo getOjInfo() {
         return HYSBZInfo.INFO;
@@ -37,33 +61,12 @@ public class HYSBZSubmitter extends CanonicalSubmitter {
 
     @Override
     protected String submitCode(SubmissionInfo info, RemoteAccount remoteAccount, DedicatedHttpClient client) {
-        for(RemoteAccountConfig acc : HYSBZCrawler.acount.accounts)
-        {
-            client.get("/JudgeOnline/logout.php", HttpStatusValidator.SC_OK);
-
-            HttpEntity entity = SimpleNameValueEntityFactory.create( //
-                    "user_id", acc.id, //
-                    "password", acc.password);
-            client.post("/JudgeOnline/login.php", entity, new SimpleHttpResponseValidator() {
-                @Override
-                public void validate(SimpleHttpResponse response) throws Exception {
-                    Validate.isTrue(response.getBody().contains("history.go(-2)"));
-                }
-            });
-            entity = SimpleNameValueEntityFactory.create(
-                    "language", info.remotelanguage, //
-                    "id", info.remoteProblemId, //
-                    "source", info.sourceCode //
-            );
-            try {
-                client.post("/JudgeOnline/submit.php", entity, HttpStatusValidator.SC_MOVED_TEMPORARILY);
-            }
-            catch (Exception ex)
-            {
-                continue;
-            }
-            break;
-        }
+        HttpEntity entity = SimpleNameValueEntityFactory.create(
+                "language", info.remotelanguage, //
+                "id", info.remoteProblemId, //
+                "source", info.sourceCode //
+        );
+        client.post("/JudgeOnline/submit.php", entity, HttpStatusValidator.SC_MOVED_TEMPORARILY);
         return null;
     }
 
